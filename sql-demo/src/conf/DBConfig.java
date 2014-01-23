b427e6c33ab2f5ad3f76c6f8b8ebb8368b62b731
Kim <kim.quhappy@gmail.com>
2014-01-23 12:15:14 +0800

    关闭数据库链接

--- sql-demo/src/conf/DBConfig.java
+++ sql-demo/src/conf/DBConfig.java
@@ -2,7 +2,7 @@ package conf;
 
 public interface DBConfig {
 	
-	public static final String URL = "jdbc:mysql://192.168.10.27:3306/quhappy3";
+	public static final String URL = "jdbc:mysql://192.168.10.27:3306/quhappy5";
 	public static final String USERNAME = "root";
 	public static final String PASSWORLD = "root";
 
--- sql-demo/src/tester/SQLAverage.java
+++ sql-demo/src/tester/SQLAverage.java
@@ -20,11 +20,12 @@ public class SQLAverage {
 
 	private static Log log = LogFactory.getLog(SQLAverage.class);
 
-	private static final int DEFAULT_TIMES = 500;
+	private static final int DEFAULT_TIMES = 1000;
 
 	public static long execute(Connection conn, String sql) {
+		Statement stmt = null;
 		try {
-			Statement stmt = conn.createStatement();
+			stmt = conn.createStatement();
 
 			Calendar cs = Calendar.getInstance(); // 开始时间
 			log.info("执行次数：" + DEFAULT_TIMES);
@@ -38,16 +39,34 @@ public class SQLAverage {
 			return rs / DEFAULT_TIMES;
 		} catch (SQLException e) {
 			log.error("创建数据库连接", e);
+		} finally {
+			if (stmt != null) {
+				try {
+					if (!stmt.isClosed()) {
+						stmt.close();
+					}
+				} catch (SQLException e) {
+					log.error("关闭Statement", e);
+				}
+			}
 		}
 		return 0;
 	}
 
 	public static void main(String[] args) {
-		String sql = " select * from qu_tb_agent_order ";
+		String sql = " select * from qu_tb_agent_order where createDate > 2013-12-01 and createDate < 2014-01-01 ";
 		log.info("SQL语句：" + sql);
 		
 		Connection conn = MySQLConnecter.getConnection();
 		long times = SQLAverage.execute(conn, sql);
 		log.info("平均耗时：" + times + "ms");
+		
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			log.error("关闭Connection", e);
+		}
 	}
 }
